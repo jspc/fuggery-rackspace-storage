7e1f584db877e2462eddcf2d60de51f7ae0126d0
jspc <james@zero-internet.org.uk>
2013-12-20 17:39:47 +0000

    WIP

--- umount.rb
+++ umount.rb
@@ -4,7 +4,13 @@ require 'fc/rack/storage'
 
 s = FC::Rack::Storage.new ENV['RACKSPACE_USER'], ENV['RACKSPACE_KEY']
 
-#s.umount 'jspc_test', 'jspc_test-db'
-snapshot = s.create_snapshot('jspc_test-db')
+#s.mount 'jspc_test', 'jspc_test-db'
+#exit
 
-puts 'done'
+s.umount 'jspc_test', 'jspc_test-db'
+
+sleep 30
+
+snapshot = s.create_snapshot('jspc_test-db', wait=true)
+s.create_volume('new_bollocks', snapshot, wait=true)
+s.mount 'jspc_test', 'new_bollocks'
